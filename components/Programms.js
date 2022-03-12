514ea1403e24667b104c6be31418babedda2085f
MatoXD99 <maticpohovski@gmail.com>
2022-03-12 12:07:11 +0100

    New images
    - New images for Figma, mySQL and NodeJS

--- components/Programms.js
+++ components/Programms.js
@@ -73,7 +73,7 @@ export default function Example() {
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src='./images/nodejs.svg'
+                src='./images/nodejs.png'
                 alt="Customer profile user interface"
               />
             </div>
@@ -98,7 +98,7 @@ export default function Example() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src='./images/flutter.png'
+                src='./images/figma.png'
                 alt="Customer profile user interface"
               />
             </div>
@@ -123,7 +123,7 @@ export default function Example() {
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src='./images/flutter.png'
+                src='./images/mysql.png'
                 alt="Customer profile user interface"
               />
             </div>
