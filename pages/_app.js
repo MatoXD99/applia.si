4c69e085785828f64ef322110de3288372edbef1
MatoXD99 <maticpohovski@gmail.com>
2022-03-14 18:43:11 +0100

    Google analytics
    - Added Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return (<Component {...pageProps} />)
+  return (
+    <>
+      <Script strategy='lazyOnLoad' src='https://www.googletagmanager.com/gtag/js?id=G-V2WELDKY45' />
+      <Script
+        strategy='lazyOnLoad'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-V2WELDKY45');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
