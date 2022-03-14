992bab427a7c4220729a4c7fc793757c29011671
MatoXD99 <maticpohovski@gmail.com>
2022-03-14 19:03:03 +0100

    Fixed gtag code
    - Added gtag script

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,42 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
-      <Script id="analytics" strategy='lazyOnLoad' src='https://www.googletagmanager.com/gtag/js?id=G-V2WELDKY45' />
-      <Script id="analytics-script"
-        strategy='lazyOnLoad'>
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-V2WELDKY45');
-        `}
-      </Script>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
