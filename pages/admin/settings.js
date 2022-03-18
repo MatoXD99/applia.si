31a15012071832af410460e691658ab4da6f2874
MatoXD99 <maticpohovski@gmail.com>
2022-03-18 14:02:32 +0100

    Deleteted preferences
    - Deleteted preferences

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Layout from '../../components/admin/Layout'
-import Preferences from '../../components/admin/Preferences'
 
 const Settings = () => {
     return (
         <Layout>
-            <Preferences />
+            Preferences
         </Layout>
     )
 }
